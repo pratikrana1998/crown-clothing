41c6b52de37665df6a027e5f51065919ad8652cd
Pratik Rana <pratikrana1433@gmail.com>
2022-12-13 22:56:17 +0530

    adding category-container for the five categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Tiered Cakes',
+    },
+    {
+      id: 2,
+      title: 'Drinks',
+    },
+    {
+      id: 3,
+      title: 'Apparel',
+    },
+    {
+      id: 4,
+      title: 'Pastry',
+    },
+    {
+      id: 5,
+      title: 'Cakes',
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /* There are five different category container with each different category at the home page */
+    /* Iterating over the categories array to optimize the code (efficient adding of new categories) */
+
+    <div className="categories-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
