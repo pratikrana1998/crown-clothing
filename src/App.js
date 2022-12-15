b8a547076e2b0fab032da318f40bb02be7bb96ab
Pratik Rana <pratikrana1433@gmail.com>
2022-12-15 09:57:31 +0530

    modifying the images at the home page & adding category-item component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 const App = () => {
 
@@ -6,27 +7,27 @@ const App = () => {
     {
       id: 1,
       title: 'Tiered Cakes',
-      imageUrl: 'https://i.ibb.co/xKvKW1X/tieredcake.png',
+      imageUrl: 'https://i.ibb.co/L1GVDPW/tieredcake.jpg',
     },
     {
       id: 2,
       title: 'Drinks',
-      imageUrl: 'https://i.ibb.co/k8PHkDn/drinks.jpg',
+      imageUrl: 'https://i.ibb.co/Wks6NcX/drinks.jpg',
     },
     {
       id: 3,
       title: 'Apparel',
-      imageUrl: 'https://i.ibb.co/99hQ2PC/apparel.jpg',
+      imageUrl: 'https://i.ibb.co/ZByJk1W/apparel.jpg',
     },
     {
       id: 4,
       title: 'Pastry',
-      imageUrl: 'https://i.ibb.co/n6Wz846/pastry.jpg',
+      imageUrl: 'https://i.ibb.co/5vwGYtX/pastry.jpg',
     },
     {
       id: 5,
       title: 'Cakes',
-      imageUrl: 'https://i.ibb.co/ZhHH6M1/oreo.jpg',
+      imageUrl: 'https://i.ibb.co/wLd79bP/cake.jpg',
     },
   ]
 
@@ -35,16 +36,8 @@ const App = () => {
     /* Iterating over the categories array to optimize the code (efficient adding of new categories) */
 
     <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }}></div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
