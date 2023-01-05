896fa8ad7cc8995504ca5a59da3ad9a224e863b9
pratikrana1433 <pratikrana1433@gmail.com>
2023-01-05 13:32:29 +0530

    adding navigation component and styling + Outlet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router";
+
+const Shop = () => {
+  return <h1>I am the Shop page.</h1>
+}
 
 const App = () => {
   return(
     /* Routes allows this application to register these Route level components that will then in turn render a specific component when it matches 
        the specific route that you're looking for */ 
     <Routes>
-      <Route path='/' element={ <Home /> } />
+      <Route path='/' element={ <Navigation /> } >
+        <Route index element={ <Home /> } />
+        <Route path='shop' element={ <Shop /> } />
+      </Route>
     </Routes>
   )
 }
