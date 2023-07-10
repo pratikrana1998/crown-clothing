722c5e1fa6f7b20ee8e180d89fea144cb8a1d920
pratikrana1433 <pratikrana1433@gmail.com>
2023-07-10 16:14:52 +0530

    created checkout page along with checkout-items and checkout-cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -8,11 +8,30 @@ const addCartItem = (cartItems, productToAdd) => {
     if(existingCartItem) {
         return cartItems.map((cartItem) => cartItem.id === productToAdd.id ?
             {...cartItem, quantity: cartItem.quantity + 1} : cartItem
-        )
+        );
     }
 
     /*Return new array with modified cartItems/ new Cart Item */
     return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    /*Find the cart item to remove */
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+    /*Check if quantity is == 1, if it is remove that item from the cart */
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    /*Return back cartitems with matching cart item with reduced quantity */
+    return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id ?
+    {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+    );
+}
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
 }
 
 export const CartContext = createContext({
@@ -20,7 +39,10 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0,
+    cartTotal: 0,
 });
 
 /*
@@ -46,6 +68,7 @@ export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     /* We pass useEffect a callback and this callback runs everytime something in our dependency array changes */
     useEffect(() => {
@@ -53,10 +76,23 @@ export const CartProvider = ({children}) => {
         setCartCount(newCartCount);
     }, [cartItems]);
 
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
+        setCartTotal(newCartTotal);
+    }, [cartItems]);
+
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
+    const removeItemFromCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    }
+
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, cartTotal, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartCount};
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
